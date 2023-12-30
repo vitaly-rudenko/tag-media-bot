012f7ae7141103a232fae92e984162f46c22621e
Vitaly Rudenko <maurice.dederick@gmail.com>
2023-12-30 14:33:39 +0200

    Added validation

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,15 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { ValidationPipe } from '@nestjs/common'
+import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-  console.log(`Application is running on: ${await app.getUrl()}`);
+  const app = await NestFactory.create(AppModule)
+
+  app.useGlobalPipes(new ValidationPipe())
+
+  await app.listen(Number(process.env.PORT) || 3000)
+
+  console.log(`Application is running on: ${await app.getUrl()}`)
 }
-bootstrap();
+
+bootstrap()
