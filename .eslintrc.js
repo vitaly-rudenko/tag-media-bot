f358b50ce6a5b5d6be1e982118e773760dbf217b
Vitaly Rudenko <maurice.dederick@gmail.com>
2023-12-31 22:44:50 +0200

    Setup nx

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+/** @type {import('eslint').ESLint.ConfigData} */
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: ['tsconfig.json'],
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    'prettier/prettier': ['error', { 'semi': false }],
+    'semi': ['error', 'never'],
+  },
+}
---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+  semi: false,
+  printWidth: 100,
+  singleQuote: true,
+  trailingComma: 'all',
+}
